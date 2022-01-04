31b44df3f8353816508c405547e80e652ad41f14
gagansingh894 <gds31.gagandeepsingh@gmail.com>
2022-01-04 23:29:30 +0530

    code for creating and running server for microservice

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	h "github.com/gagansingh894/go-urlshortner/api"
+	mr "github.com/gagansingh894/go-urlshortner/repository/mongodb"
+	rr "github.com/gagansingh894/go-urlshortner/repository/redis"
+
+	"github.com/gagansingh894/go-urlshortner/shortner"
+)
+
+func main() {
+	//	repo <- service -> serializer -> http
+
+	repo := chooseRepo()
+	service := shortner.NewRedirectService(repo)
+	handler := h.NewHandler(service)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+
+	errs := make(chan error, 2)
+	go func() {
+		fmt.Println("Listening on port :8000")
+		errs <- http.ListenAndServe(httpPort(), r)
+
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	fmt.Printf("Terminated %s", <-errs)
+}
+
+func httpPort() string {
+	port := "8008"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	return fmt.Sprintf(":%s", port)
+}
+
+func chooseRepo() shortner.RedirectRepository {
+	switch os.Getenv("URL_DB") {
+	case "redis":
+		redisURL := os.Getenv("REDIS_URL")
+		repo, err := rr.NewRedisRepository(redisURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	case "mongodb":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongodb := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+		repo, err := mr.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	}
+	return nil
+}
